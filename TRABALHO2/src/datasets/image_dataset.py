74cd0d99d38910cd0458ab7a3fa5fb96c9281cba
BetimMontagens <117229233+BetimMontagens@users.noreply.github.com>
2023-02-01 17:53:26 -0300

    Image Dataset feito
    image dataset feito... falta o dataset de texto, provavelmente funciona de forma parecida com esse dataset.

--- TRABALHO2/src/datasets/image_dataset.py
+++ TRABALHO2/src/datasets/image_dataset.py
@@ -1,9 +1,7 @@
 
 from typing import Tuple, Any, Dict
 from .dataset_interface import DatasetInterface
-import csv
-
-# recebido dependendo do tipo de teste sendo efetuado
+import cv2
 
 class ImageDataset(DatasetInterface):
     def __init__(self, path: str) -> None:
@@ -19,14 +17,14 @@ class ImageDataset(DatasetInterface):
         return len(self.image_classes)
 
     def get(self, idx: int) -> Tuple[Any, str]: #vetorizar imagem e dps vc se vira boco
-        return cv2.imread(self.image_classes[idx][0]), self.image_classes[idx][1]
-        # ler a i-esima imagem do disco usando a biblioteca cv2 e retornar
-        # a imagem e a respectiva classe
+        for i in range (len(self.image_classes)):
+            if 'test.txt' in self.path:
+                self.path= self.path.replace('test.txt', self.image_classes[i][0])
+            else:
+                self.path= self.path.replace('train.txt', self.image_classes[i][0])
 
-        #ler o path, se tiver por exp text.txt, remover o text.txt do path
-        #dps tem q adicionar o nome da imagem, sem a classe, ao path
-        #(fazer isso com dois indices, um para o indice da lista dentro dos vetores e
-        # depois outro indice pra escolher o primeiro espaço dessa lista (nome do arquivo))
-
-        #data/datasets/img_small/test.txt
-        #data/datasets/img_small/test/000.png
+            images_classes = []
+        for line in self.image_classes:
+            image, class_ = line
+            images_classes.append((cv2.imread(image), class_))
+        return images_classes
