2d6e9b7f31b31a6faf27f3822a92c04cab471c7e
OrgulhoPK <arthurfiorio10@gmail.com>
2023-02-06 15:53:48 -0300

    mudanças básicas sobre oq temos q fazer

--- TRABALHO2/src/datasets/news_dataset.py
+++ TRABALHO2/src/datasets/news_dataset.py
@@ -17,6 +17,11 @@ class NewsDataset(DatasetInterface):
         return len(self.text_classes)
 
     def get(self, idx: int) -> Tuple[Any, str]:
+        self.superlista=[]
+        for i in range(len(self.text_classes)):
+
+        # fazer aquela parada de escrever todas as palavras nessa superlista, somente caso essa palavra nao esteja
+        # previamente nessa lista, remover todas as palavras da stopwords e contar a frenquencia delas
         # to do: de forma parecida com o dataset de imagens, temos que retornar uma lista que contem
         # diversas tuplas que contem o texto do arquivo de forma vetorizada(n sei como vetorizar, é mais dificil
         # do que a de imagens, e mais chata de acordo com o antonio), e também sua classe, por exp.:
@@ -24,4 +29,4 @@ class NewsDataset(DatasetInterface):
         
         # ler a i-esima noticia do disco e retornar o texto como uma string e
         # a classe
-        return 0, ""
+            return 0, ""
